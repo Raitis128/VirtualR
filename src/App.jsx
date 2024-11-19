b3cdd10b17b637e189a77354962d28c1803e7bdb
Raitis Iziks <raitisiziks@gmail.com>
2024-11-19 18:23:32 +0200

    Implement Testimonials component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import FeaturesSection from "./components/FeaturesSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 import Workflow from "./components/Workflow";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <FeaturesSection />
         <Workflow />
         <Pricing />
+        <Testimonials />
       </div>
     </>
   );
